11f6f25dfe6c7144438964efb641949620d2471e
Dapo22 <dapoodumade@gmail.com>
2022-12-08 14:32:39 -0800

    task 7

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,8 @@
-#include <stdio.h>                                                                                                                      
-/** 
+#include <stdio.h>                                                                      /** 
  * main - Entry
  *
  * Return: Always 0
  */
-
 int main(void)
 {
 	printf("Size of a char: %lu byte(s)\n", sizeof(char));
