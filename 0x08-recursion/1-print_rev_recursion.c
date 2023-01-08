35ecb93f0b97e42c9785c8fedca867cefb50d969
Dapo22 <dapoodumade@gmail.com>
2023-01-08 04:52:07 -0800

    task 1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,16 +1,14 @@
 #include "main.h"
 /**
- * _puts_recursion - prints a string. followed by a new line
- * @s: string
+ *_print_rev_recursion - prints a string in reverse
+ * @s: pointer to block of memory to fill
  * Return: no return
  */
 void _print_rev_recursion(char *s)
 {
-	if (*s != '\0')
+	if (*s)
 	{
+		_print_rev_recursion(s + 1);
 		_putchar(*s);
-		_puts_recursion(s + 1);
 	}
-	else
-		_putchar('\n');
 }
