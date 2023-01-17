c9ddc0ee087a9b72f14c3e22add0a9720cb07426
Dapo22 <dapoodumade@gmail.com>
2023-01-17 09:40:52 -0800

    task 0

---
+++ 0x0E-structures_typedef/_putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * _putchar - writes tge character c to stdout
+ * @c: the character to print
+ *
+ * Return: On success 1
+ * On error, -1 is returned, and error is set appropriately
+ */
+int _putchar(char c)
+{
+	return (write(, &c, 1));
+}
